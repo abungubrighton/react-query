044247931374b9501350aa88dafd928df912cde0
abungubrighton <brighton@ibuqacapital.com>
2024-02-18 12:36:05 +0300

    chore:handling mutations

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -1,10 +1,19 @@
-import { useQuery } from "react-query";
-import { fetchTodos } from "./api";
+import { useQuery ,useMutation} from "react-query";
+import { addTodo, fetchTodos } from "./api";
 import TodoCard from "./components/TodoCard";
+import { useState } from "react";
 export default function Demo() {
+  // form state
+  const [title, setTitle] = useState("")
+  // Query todos
   const { data: todos, isLoading } = useQuery({
     queryFn: ()=> fetchTodos(),
     queryKey:['todos']
+  });
+
+// make changes to DB or object in memory
+  const { mutateAsync: addTodoMutation } = useMutation({
+    mutationFn:addTodo
   });
 
   if (isLoading) { 
@@ -13,6 +22,17 @@ export default function Demo() {
 
   return (
     <div>
+      <div>
+        <input  type="text" name="todo" id="todo" onChange={(e) => setTitle(e.target.value)} value={title} />
+        <button onClick={async () => {
+          try {
+            await addTodoMutation({ title });
+            setTitle("")
+          } catch (error) {
+            console.log(error);
+          }
+        }}>Add Todo</button>
+      </div>
       {todos?.map(todo => {
         return <TodoCard key={todo.id} todo={todo} />
       })}
